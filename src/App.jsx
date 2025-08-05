36f49e638f4000d723a5ef84d6c5a3a19fe1efb0
abrarasif11 <faasif929@gmail.com>
2025-08-05 15:27:26 +0600

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
-import './App.css'
+import "./App.css";
+import Navbar from "./Component/Navbar/Navbar";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-2xl bg-amber-200 '>Price Option</h1>
+    <Navbar/>
+      <h1 className="text-2xl bg-amber-200 ">Price Option</h1>
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Component/Navbar/PriceOptions/PriceOptions.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const PriceOptions = () => {
+    [
+        {
+          "id": 1,
+          "name": "Basic Plan",
+          "price": 25.99,
+          "features": [
+            "Access to all gym equipment",
+            "Free water station",
+            "Locker room access"
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Standard Plan",
+          "price": 49.99,
+          "features": [
+            "Access to all gym equipment",
+            "Group fitness classes",
+            "2 personal training sessions",
+            "Locker and shower access"
+          ]
+        },
+        {
+          "id": 3,
+          "name": "Premium Plan",
+          "price": 79.99,
+          "features": [
+            "24/7 unlimited access",
+            "All group classes included",
+            "5 personal training sessions",
+            "Sauna and spa access",
+            "Free diet and fitness consultation"
+          ]
+        }
+      ]
+      
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PriceOptions
