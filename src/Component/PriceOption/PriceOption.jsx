75b7fb476a80f4c087b68a1f9a84b127fb8e6051
abrarasif11 <faasif929@gmail.com>
2025-08-05 16:18:53 +0600

    features-added

--- src/Component/PriceOption/PriceOption.jsx
+++ src/Component/PriceOption/PriceOption.jsx
@@ -1,56 +1,36 @@
 import React from 'react'
+import Features from '../Features/Features'
 
-const PriceOption = ({option}) => {
+const PriceOption = ({ option }) => {
   return (
-    <section className="py-20  text-gray-800">
-    <div className="container px-4 mx-auto">
-      
-      <div className="flex flex-wrap items-stretch -mx-4">
-        <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
-         
-        </div>
-        <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
+    <section className="py-10 text-gray-800">
+      <div className="px-4 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
           <div className="flex flex-grow flex-col p-6 space-y-6 rounded shadow sm:p-8 bg-violet-600 text-gray-50">
             <div className="space-y-2">
               <h4 className="text-2xl font-bold">{option.name}</h4>
-              <span className="text-6xl font-bold">{option.price}
+              <span className="text-6xl font-bold">
+                {option.price}
                 <span className="text-sm tracking-wide">/month</span>
               </span>
             </div>
-            <p className="leading-relaxed">Morbi cursus ut sapien sit amet consectetur.</p>
-            <ul className="flex-1 space-y-2">
-              <li className="flex items-center space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6">
-                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
-                </svg>
-                <span>Everything in Free</span>
-              </li>
-              <li className="flex items-center space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6">
-                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
-                </svg>
-                <span>Phasellus tellus</span>
-              </li>
-              <li className="flex items-center space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6">
-                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
-                </svg>
-                <span>Praesent faucibus</span>
-              </li>
-              <li className="flex items-center space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6">
-                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
-                </svg>
-                <span>Aenean et lectus blandit</span>
-              </li>
-            </ul>
-            <a rel="noopener noreferrer" href="#" className="inline-block w-full px-5 py-3 font-bold tracking-wider text-center rounded bg-gray-100 text-violet-600">Get Started</a>
+            <p className="leading-relaxed">Special Features</p>
+            <div className="flex-grow">
+              {option.features.map((feature, index) => (
+                <Features key={index} feature={feature} />
+              ))}
+            </div>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="inline-block w-full px-5 py-3 font-bold tracking-wider text-center rounded bg-gray-100 text-violet-600"
+            >
+              Get Started
+            </a>
           </div>
         </div>
-        
       </div>
-    </div>
-  </section>
+    </section>
   )
 }
 
