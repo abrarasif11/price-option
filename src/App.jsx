5a9c69e92719e85b562903536457368bca552aea
abrarasif11 <faasif929@gmail.com>
2025-08-05 12:52:40 +0600

    added-tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Price Option</h1>
+      <h1 className='text-2xl bg-amber-200 '>Price Option</h1>
     </>
   )
 }
