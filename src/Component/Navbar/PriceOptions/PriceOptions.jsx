5bd3069705bdb12280cf623883c06e35f0aed7e8
MD Fahim Abrar Asif <faasif929@gmail.com>
2025-09-28 13:36:50 +0600

    Update Price Text

--- src/Component/Navbar/PriceOptions/PriceOptions.jsx
+++ src/Component/Navbar/PriceOptions/PriceOptions.jsx
@@ -40,7 +40,7 @@ const PriceOptions = () => {
 
   return (
     <div>
-      <h2 className='text-3xl font-semibold'>Best Price in this Universe</h2>
+      <h2 className='text-3xl font-semibold'>Best Price in this Town</h2>
       <div className='grid grid-cols-3 gap-6'>
       {
         priceOptions.map((option) => (
